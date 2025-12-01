bc25b0e4736abe16cbf6cb6576941dfabec596bb
fher03 <feransic@gmail.com>
2025-11-30 18:00:35 -0600

    feat: Se implementa seeder de la base de datos

---
+++ server/src/database/queries/seed.sql
@@ -0,0 +1,11 @@
+-- Users
+
+INSERT INTO users (username, password) VALUES ('Diego', 'abc123');
+
+INSERT INTO
+    users (username, password, role)
+VALUES ('Fernando', 'abc123', 'admin');
+
+--Products
+
+INSERT INTO products (name, price) VALUES ('Lata de Refresco', 17.22)
